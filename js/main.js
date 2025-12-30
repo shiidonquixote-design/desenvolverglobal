64af16d0b41334fd1c94ca719360cb8a862b0643
shiidonquixote-design <shiidonquixote@gmail.com>
2025-12-30 19:46:30 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+// cek mode tersimpan
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙";
+  }
+});
